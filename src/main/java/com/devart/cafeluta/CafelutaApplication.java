8aa9a5dcc4c35f917ff0752b323c9ebc4cdcad32
Alexandru Contu <alexandru.contu99@gmail.com>
2022-07-07 13:30:16 +0300

    Initial commit

---
+++ src/main/java/com/devart/cafeluta/CafelutaApplication.java
@@ -0,0 +1,13 @@
+package com.devart.cafeluta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafelutaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CafelutaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devart/cafeluta/CafelutaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devart.cafeluta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafelutaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
